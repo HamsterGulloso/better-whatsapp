5b71db3586f6f167c7fdbb2b5e61af17041f8a71
HamsterGulloso <danielgullovasconcelos@gmail.com>
2025-10-27 17:43:45 -0300

    Better build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 if [ "$1" == "firefox" ]; then
     cp ./manifest.firefox.json ./manifest.json
-    zip -r ./better-whatsapp.xpi *
+    if [ -f ./better-whatsapp.xpi ]; then
+        rm ./better-whatsapp.xpi
+    fi
+    zip -r ./better-whatsapp.xpi ./main.css ./manifest.json
 elif [ "$1" == "chrome" ]; then
     cp ./manifest.chrome.json ./manifest.json
 else
